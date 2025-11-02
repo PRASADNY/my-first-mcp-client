0486b286e5ce239c067dcaf6b672e2d4485a7737
PRASADNY <PRASAD.NY@GMAIL.COM>
2025-11-02 16:52:04 -0500

    Update client.py to use command line argument for server

--- client.py
+++ client.py
@@ -111,15 +111,15 @@ class MCPClient:
 
 
 async def main():
-    #if len(sys.argv) < 2:
-     #   print("Usage: python client.py <path_to_server_script>")
-    #sys.exit(1)
+    if len(sys.argv) < 2:
+       print("Usage: python client.py <path_to_server_script>")
+    sys.exit(1)
 
-  #  print(sys.argv[1])
+    print(sys.argv[1])
 
     client = MCPClient()
     try:
-        await client.connect_to_server('..\\my-first-mcp-server\\main.py')
+        await client.connect_to_server(sys.argv[1])
         await client.chat_loop()
     finally:
         await client.cleanup()
